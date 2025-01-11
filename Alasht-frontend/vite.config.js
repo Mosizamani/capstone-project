a056c0e6435adbb29c6c1a3c0f69748092fae806
Mosi Zamani <eng.mzamanit@gmail.com>
2025-01-11 08:18:43 -0800

    unnecessary files are removed form the frontend code

--- Alasht-frontend/vite.config.js
+++ Alasht-frontend/vite.config.js
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     // Proxy all requests starting with '/api' to your backend running on localhost:5000
-  //     '/': 'http://localhost:4001', // Adjust if your backend runs on a different port
-  //   },
-  // },
 })
