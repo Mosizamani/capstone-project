16386ae27ac81919c19fc2af485bda364dc3aa96
Mosi Zamani <eng.mzamanit@gmail.com>
2025-01-12 14:07:04 -0800

    some adjustment to CORS settings, to solve the problem.

--- Alasht-backend/index.js
+++ Alasht-backend/index.js
@@ -64,6 +64,18 @@ app.use((req, res, next) => {
     next()
 })
 
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', frontEndUrl);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        return res.sendStatus(204); // Respond with no content for preflight
+    }
+    next();
+});
+
+
 // Routers
 app.use(proRouter)
 app.use(clientRouter)
