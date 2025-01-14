3fabe5cbfc10d8f4abdcf9233bc71a82b6a70cdb
Mosi Zamani <eng.mzamanit@gmail.com>
2025-01-13 22:09:33 -0800

    The problem with cors is solved. now try to add content to the frontend and do some css to it

--- Alasht-frontend/src/components/Home.jsx
+++ Alasht-frontend/src/components/Home.jsx
@@ -2,8 +2,12 @@
 export default function Home() {
 return (
     <div>
-    <h1>Home</h1>
-    <p>Welcome to the Home page!</p>
+    <h1>Alasht</h1>
+    <p>Alasht Application for Connecting Contractors, Subcontractors, Tradespeople, and Clients. 
+        This app is a comprehensive platform designed to streamline the process of finding, 
+        hiring, and collaborating with contractors, subcontractors, tradespeople, and clients on a variety of projects. 
+        By connecting skilled professionals with project owners, the app bridges the gap between demand and expertise 
+        in industries like construction, repair, and home improvement, fostering efficient, reliable, and high-quality project management.</p>
     </div>
 )
 }
