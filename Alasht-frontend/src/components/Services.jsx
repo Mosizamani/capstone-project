f2e69ed30a23edbdb132b3fcfadc4082cad2ea2e
Mosi Zamani <eng.mzamanit@gmail.com>
2025-01-13 23:20:03 -0800

    some content aded to the service component

--- Alasht-frontend/src/components/Services.jsx
+++ Alasht-frontend/src/components/Services.jsx
@@ -3,7 +3,16 @@ export default function Services() {
     return (
         <div>
             <h1>Services</h1>
-            <p>Welcome to the Services page!</p>
+            <p>We offer a comprehensive range of services related to building and home improvement, designed to meet all your construction 
+                and renovation needs. Whether you're looking to create a custom play area for your children with indoor rock climbing walls, 
+                gym equipment, or wooden playhouses, or you're in need of larger-scale building services, we've got you covered. Our team 
+                specializes in crafting high-quality, tailored solutions that enhance both the functionality and aesthetic of your home.</p>
+
+            <p>From initial design to final installation, we ensure every aspect of your project is handled with professionalism and 
+                attention to detail. We provide services such as custom building, assembly, and installation of various home features, 
+                including climbing walls, gyms, and playhouses. Our experts work closely with you to bring your vision to life, ensuring 
+                a seamless experience from start to finish. Whether it’s a small home improvement project or a larger renovation, we deliver 
+                top-notch results that exceed expectations.</p>
         </div>
     )
 }
