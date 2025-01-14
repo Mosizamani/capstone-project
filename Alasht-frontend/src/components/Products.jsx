aabca7231eb410efb8a4b2d374f50393dad6d388
Mosi Zamani <eng.mzamanit@gmail.com>
2025-01-13 23:01:11 -0800

    some content aded to the products component

--- Alasht-frontend/src/components/Products.jsx
+++ Alasht-frontend/src/components/Products.jsx
@@ -2,7 +2,17 @@ export default function Products() {
     return (
         <div>
             <h1>Products</h1>
-            <p>Welcome to the Products page!</p>
+            <p>We offer a wide range of premium products designed for babies and young children, including indoor rock climbing walls, 
+                ladder walls, gyms and climbers, and wooden playhouse kits. Each product is thoughtfully crafted to inspire active play, 
+                creativity, and fun in a safe environment.</p>
+            <p>Our process is simple and customer-focused:</p>
+            <p>Custom Order: Select the product that best suits your needs from our product catalog.</p>
+            <p>Made-to-Order: We build your chosen product with care and precision upon receiving your order.</p>
+            <p>Delivery & Installation: We deliver it to your location and send a specialist to professionally assemble and install it, 
+                ensuring everything is perfectly set up for your little ones to enjoy.</p>
+            <p>Below is our complete product list with corresponding prices for your convenience. </p>
+            <p>Feel free to reach out with any questions or to place an order. 
+                We're here to help create a playful and enriching space for your family!</p>
         </div>
     )
 }
